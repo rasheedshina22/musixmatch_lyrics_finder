61bb33bfede5c29b04f02d5b1dd3f6dc3b08d95d
rasheedshina22 <rasheedshinaopeyemi@gmail.com>
2019-06-25 05:53:16 -0700

    added the build folder

---
+++ build/precache-manifest.5b4f41e6b984a9ddabaa99fe2fed8f19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6425bd630c001e6829b8a16fdd30bf",
+    "url": "/lyricsfinder/index.html"
+  },
+  {
+    "revision": "3f3d83b9fcbd92633344",
+    "url": "/lyricsfinder/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f207da0d6525e11aff33",
+    "url": "/lyricsfinder/static/js/2.fbc50638.chunk.js"
+  },
+  {
+    "revision": "3f3d83b9fcbd92633344",
+    "url": "/lyricsfinder/static/js/main.3a607cc5.chunk.js"
+  },
+  {
+    "revision": "477a4ea197ae4bf199ab",
+    "url": "/lyricsfinder/static/js/runtime~main.a9ea7de9.js"
+  }
+]);
