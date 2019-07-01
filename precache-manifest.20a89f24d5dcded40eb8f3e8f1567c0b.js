b2cb1b32e13a029aae902bc7f4a8b070f56192b1
rasheedshina22 <rasheedshinaopeyemi@gmail.com>
2019-07-01 12:57:08 -0700

    Updates

---
+++ precache-manifest.20a89f24d5dcded40eb8f3e8f1567c0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7aa8ae87908ab2f467f4b59022664d",
+    "url": "/musixmatch_lyrics_finder/index.html"
+  },
+  {
+    "revision": "058117b862ee89df08fd",
+    "url": "/musixmatch_lyrics_finder/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "40c13f2a3098f62384b1",
+    "url": "/musixmatch_lyrics_finder/static/js/2.b669e78e.chunk.js"
+  },
+  {
+    "revision": "058117b862ee89df08fd",
+    "url": "/musixmatch_lyrics_finder/static/js/main.0b137c61.chunk.js"
+  },
+  {
+    "revision": "ec6fa812ad526e268ade",
+    "url": "/musixmatch_lyrics_finder/static/js/runtime~main.3feab6ce.js"
+  }
+]);
